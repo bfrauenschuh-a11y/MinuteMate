bd1b33043075aa6b35ee108925572581989d03da
bfrauenschuh-a11y <b.frauenschuh@outlook.com>
2025-09-17 14:18:44 +0200

    Update UI and backend for meeting minutes app

--- server.js
+++ server.js
@@ -3,12 +3,19 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const actionItemsRoutes = require('./routes/actionItems');
 
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+
 app.use('/api/action-items', actionItemsRoutes);
+
+app.get('/', (req, res) => {
+  res.send('✅ MinuteMate backend is running');
+});
+
 
 app.listen(3001, () => console.log('Server running on port 3001'));
